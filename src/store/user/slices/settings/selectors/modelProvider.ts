68751811649c306a25cf7b872934c262e4b66349
Francisco Trillo <ftrillo@efitherma.com>
2024-05-17 12:59:34 +0200

    whitelist vision

--- src/store/user/slices/settings/selectors/modelProvider.ts
+++ src/store/user/slices/settings/selectors/modelProvider.ts
@@ -110,7 +110,7 @@ const isModelEnabledFunctionCall = (id: string) => (s: UserStore) =>
 // vision model white list, these models will change the content from string to array
 // refs: https://github.com/lobehub/lobe-chat/issues/790
 const isModelEnabledVision = (id: string) => (s: UserStore) =>
-  getModelCardById(id)(s)?.vision || id.includes('vision');
+  getModelCardById(id)(s)?.vision || id.includes('vision') || id.includes('firellava');
 
 const isModelEnabledFiles = (id: string) => (s: UserStore) => getModelCardById(id)(s)?.files;
 
